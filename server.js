9a99b6854fdd5aab3d41e686e25bb0d047ff7dcc
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-02-04 16:41:25 +0900

    server 구현

---
+++ server.js
@@ -0,0 +1,16 @@
+// require: nodejs가 가지고 있는 모듈을 가져오는 함수
+// http라는 모듈을 node에서 제공함
+let http = require("http");
+
+function onRequest(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("Hello Node.js");
+  res.end();
+}
+/**
+ * createServer: 서버를 만드는 함수 제공
+ * params: 실행할 함수
+ * listen: 서버는 들을 수 있음, 듣고 싶은 주파수를 인수로 넣으면 됨
+ */
+http.createServer(onRequest).listen(8888);
+// 서버 구동은 node {파일명.확장자} -> 이 경우 node server.js

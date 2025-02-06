cee5d572a7c9aca3ab5a385bf53758254a784076
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-02-06 12:53:12 +0900

    Url에 따라 프론트엔드에 다른 response 보내기

--- requestHandler.js
+++ requestHandler.js
@@ -1,20 +1,30 @@
+function responseHandler(res, message) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(message);
+  res.end();
+}
+
 function main(res) {
   console.log("main");
 
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("Main page");
-  res.end();
+  responseHandler(res, "Main page");
 }
+
 function login(res) {
   console.log("login");
 
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("Login page");
-  res.end();
+  responseHandler(res, "Login page");
+}
+
+function my(res) {
+  console.log("login");
+
+  responseHandler(res, "CHOI IN AE page");
 }
 
 let handle = {};
 handle["/"] = main;
 handle["/login"] = login;
+handle["/my"] = my;
 
 exports.handle = handle;

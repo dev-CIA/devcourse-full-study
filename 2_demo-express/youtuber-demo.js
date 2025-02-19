ff7b6ec7d0ccfb4debeb9b17ae0d41864acddb56
Inae Choi <inaechoi.dev@gmail.com>
2025-02-19 23:41:32 +0900

    express study

--- 2_demo-express/youtuber-demo.js
+++ 2_demo-express/youtuber-demo.js
@@ -35,7 +35,17 @@ db.set(id++, youtuber2);
 db.set(id++, youtuber3);
 
 app.get("/youtubers", (req, res) => {
-  res.json(Object.fromEntries(db));
+  // res.json(Object.fromEntries(db));
+  if (!db.size) {
+    res.json({ message: "" });
+    return;
+  }
+
+  const youtubers = {};
+  db.forEach((value, key) => {
+    youtubers[key] = value;
+  });
+  res.json(youtubers);
 });
 
 app.get("/youtubers/:id", function (req, res) {
@@ -52,7 +62,6 @@ app.get("/youtubers/:id", function (req, res) {
 
 app.use(express.json()); // http의 모듈인 '미들웨어': json 설정
 app.post("/youtubers", (req, res) => {
-  console.log(req.body);
   const newYoutuber = {
     ...req.body,
     sub: 0,
@@ -62,5 +71,43 @@ app.post("/youtubers", (req, res) => {
 
   res.json({
     message: `${newYoutuber.channelTitle}님, 유튜버 생활을 응원합니다!`,
+  });
+});
+
+app.delete("/youtubers/:id", (req, res) => {
+  const id = +req.params.id;
+  const youtuber = db.get(id) ?? null;
+
+  if (youtuber) db.delete(id);
+
+  res.json({
+    message: youtuber
+      ? `${youtuber.channelTitle}님, 아쉽지만 다음에 또 만나요.`
+      : `요청하신 ${id}는 없습니다. 확인해주세요.`,
+  });
+});
+
+app.delete("/youtubers", (req, res) => {
+  const message = db.size
+    ? "전체 유튜버가 삭제되었습니다."
+    : "삭제할 유튜버가 없습니다.";
+
+  if (db.size) db.clear();
+
+  res.json({ message });
+});
+
+app.put("/youtubers/:id", (req, res) => {
+  const id = +req.params.id;
+  const youtuber = db.get(id) ?? null;
+  const prevChannelTitle = youtuber.channelTitle;
+  const newChannelTitle = req.body.channelTitle;
+
+  db.set(id, { ...youtuber, channelTitle: newChannelTitle });
+
+  res.json({
+    message: youtuber
+      ? `${prevChannelTitle}님, ${newChannelTitle}(으)로 수정되었습니다.`
+      : "없는 유튜버입니다.",
   });
 });

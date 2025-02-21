c9629bd5ad171f02abd5c9aadccc86bb1ea0620b
Inae Choi <inaechoi.dev@gmail.com>
2025-02-22 00:26:59 +0900

    회원 api 수정

--- 3_youtuber-demo/user-demo.js
+++ 3_youtuber-demo/user-demo.js
@@ -6,21 +6,40 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
 });
+app.use(express.json());
 
 const usersDb = new Map();
 
-app.use(express.json());
+function isAllExisted(obj, inputCount) {
+  if (obj.constructor !== Object) return false;
+  if (Object.keys(obj).length !== inputCount) return false;
+
+  return true;
+}
 
 // 로그인
 app.post("/login", (req, res) => {
-  const user = req.body;
+  const { userId: id, password } = req.body;
 
-  if (!user || !user.id || !user.password) {
-    res.status(400).json({ message: "잘못 입력하셨습니다." });
+  if (!isAllExisted(req.body, 2)) {
+    res.status(400).json({ message: "입력하지 않은 값이 있습니다." });
     return;
   }
 
-  const name = usersDb.get(user.id).name;
+  if (id !== usersDb.get(id)?.userId) {
+    res.status(400).json({ message: "없는 유저입니다." });
+    return;
+  }
+
+  if (
+    id === usersDb.get(id)?.userId &&
+    password !== usersDb.get(id)?.password
+  ) {
+    res.status(400).json({ message: "비밀번호를 잘못 입력하셨습니다." });
+    return;
+  }
+
+  const name = usersDb.get(id).name;
   res.json({ message: `${name}님 환영합니다` });
 });
 
@@ -28,38 +47,38 @@ app.post("/login", (req, res) => {
 app.post("/join", (req, res) => {
   const user = req.body;
 
-  if (!user || !user.id || !user.password || !user.name) {
-    res.status(400).json({ message: "잘못 입력하셨습니다." });
+  if (!isAllExisted(req.body, 3)) {
+    res.status(400).json({ message: "입력하지 않은 값이 있습니다." });
     return;
   }
 
-  usersDb.set(user.id, user);
+  usersDb.set(user.userId, user);
+
   res.status(201).json({ message: `${user.name}님 환영합니다` });
 });
 
-// 회원 개별 조회
-app.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const user = usersDb.get(id);
+app
+  .route("/users/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    const user = usersDb.get(id);
 
-  if (!user) {
-    res.status(404).json({ message: "없는 회원입니다." });
-    return;
-  }
+    if (!user) {
+      res.status(404).json({ message: "없는 회원입니다." });
+      return;
+    }
 
-  res.json({ id: user.id, name: user.name });
-});
+    res.json({ id: user.id, name: user.name });
+  })
+  .delete((req, res) => {
+    const id = req.params.id;
+    const user = usersDb.get(id);
 
-// 회원 개별 탈퇴
-app.delete("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const user = usersDb.get(id);
+    if (!user) {
+      res.status(404).json({ message: "없는 회원입니다." });
+      return;
+    }
 
-  if (!user) {
-    res.status(404).json({ message: "없는 회원입니다." });
-    return;
-  }
-
-  usersDb.delete(id);
-  res.json({ message: `${user.name}님 다음에 또 뵙겠습니다` });
-});
+    usersDb.delete(id);
+    res.json({ message: `${user.name}님 다음에 또 뵙겠습니다` });
+  });

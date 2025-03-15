d381d9fccf6a7454542c7a64af265cab5fda0249
Inae Choi <inaechoi.dev@gmail.com>
2025-03-16 01:15:57 +0900

    feat: 페이징 추가

--- 4_book-shop/controller/BookController.js
+++ 4_book-shop/controller/BookController.js
@@ -3,26 +3,32 @@ const { StatusCodes } = require("http-status-codes");
 const errorHandler = require("../util/errorHandler");
 
 const books = (req, res) => {
-  const { category_id } = req.query;
+  const { category_id, news, limit, current_page } = req.query;
+  const offset = limit * (current_page - 1);
+
+  let sql = `SELECT * FROM books`;
+  const values = [];
 
   if (category_id) {
-    const sql = `SELECT * FROM books WHERE category_id = ?`;
-
-    conn.query(sql, category_id, (err, results) => {
-      if (errorHandler.isResError(err, res)) return;
-
-      const books = results.length ? results : null;
-
-      if (!books) return res.status(StatusCodes.NOT_FOUND).end();
-
-      res.status(StatusCodes.OK).json(results);
-    });
-    return;
+    sql += ` WHERE category_id = ?`;
+    values.push(category_id);
   }
-  const sql = `SELECT * FROM books`;
 
-  conn.query(sql, (err, results) => {
+  if (category_id && news)
+    sql += ` AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+
+  if (!category_id && news)
+    sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+
+  sql += ` LIMIT ? OFFSET ?`;
+  values.push(+limit, +offset);
+
+  conn.query(sql, values, (err, results) => {
     if (errorHandler.isResError(err, res)) return;
+
+    const books = results.length ? results : null;
+
+    if (!books) return res.status(StatusCodes.NOT_FOUND).end();
 
     res.status(StatusCodes.OK).json(results);
   });
@@ -31,7 +37,8 @@ const books = (req, res) => {
 const bookDetail = (req, res) => {
   const { id } = req.params;
 
-  const sql = `SELECT * FROM books WHERE id = ?`;
+  const sql = `SELECT * FROM BookShop.books LEFT JOIN category 
+                ON books.category_id = category.id WHERE books.id = ?`;
 
   conn.query(sql, id, (err, results) => {
     if (errorHandler.isResError(err, res)) return;
